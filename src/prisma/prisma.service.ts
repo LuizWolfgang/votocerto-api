f6099f390d73f77892f030e77d951831b36183e0
LuizWolfgang <luizandredev@gmail.com>
2026-02-25 18:36:07 -0300

    fix: log connect prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ['info', 'warn', 'error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('✅ Prisma conectado ao PostgreSQL');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
